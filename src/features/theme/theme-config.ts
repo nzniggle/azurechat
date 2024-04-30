f34dd026a43eb35208ad0cc3b4133007128ea381
Nigel <nigel.stevenson@minterellison.co.nz>
2024-04-30 15:42:43 +1200

    AIME icon and default prompt

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
-export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
+export const AI_NAME = "AIME";
+export const AI_DESCRIPTION = "I am a friendly AI assistant. Start a new chat by selecting a Persona and I will assist you in the style and tone that best suits your needs. Please remember to try my standard prompts, they have been carefully created by your peers.";
+export const CHAT_DEFAULT_PERSONA = "AIME";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a very friendly assistant called AIME. You live in New Zealand. Ask for more details if you need it to respond.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
